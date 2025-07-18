f3a09c71f6c2a21528505533f7ebea694aebd0fb
SharikovStepan <stepkoy@live.com>
2025-07-18 04:06:28 +0300

    work on datas 1

--- api/getTelegram.js
+++ api/getTelegram.js
@@ -1,5 +1,5 @@
 import { Redis } from "@upstash/redis";
-
+import { randomUUID } from "crypto";
 const redis = Redis.fromEnv();
 
 function setCorsHeaders(res) {
@@ -16,8 +16,40 @@ export default async function handler(req, res) {
   }
   setCorsHeaders(res);
 
-  const body = req.body;
-  console.log("bodybodybody", JSON.stringify(body));
+  const longUuid = randomUUID();
+  const buf = Buffer.from(longUuid.replace(/-/g, ""), "hex");
+  let b64 = buf.toString("base64");
 
-  return res.status(200).json({ ok: true });
+  b64 = b64.replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, ""); // делаем URL-safe: + → -, / → _, убираем = в конце
+
+  const uuid = b64;
+
+  try {
+    const body = req.body;
+    const eventName = body.caption;
+    const data = body.jsonData;
+    const eventStart = getEventTime(data, false);
+    const lastUpdate = getEventTime(data, true);
+
+    console.log("body", { eventName, eventStart, lastUpdate, data });
+
+    return res.status(200).json({ ok: true });
+  } catch (error) {}
 }
+
+function getEventTime(data, reverse) {
+  if (!data.heats) return null;
+
+  // Получаем список ключей heats и сортируем их как числа
+  const heatKeys = Object.keys(data.heats).sort((a, b) => Number(a) - Number(b));
+  if (heatKeys.length === 0) return null;
+
+  const heat = data.heats[heatKeys[reverse ? heatKeys.length - 1 : 0]];
+  if (!heat.rounds || heat.rounds.length === 0) return null;
+
+  const round = heat.rounds[reverse ? heat.rounds.length - 1 : 0];
+  const formatedTime = round.start_time_formatted || null;
+
+  return formatedTime || null;
+}
+
